fd486f8638f97dc1f7c80c9b9ceb8d33bf5a55ea
maurocelcan <90287144+maurocelcan@users.noreply.github.com>
2022-11-26 02:21:03 -0300

    1 file modified

--- src/app/song-list/song-list.component.ts
+++ src/app/song-list/song-list.component.ts
@@ -5,56 +5,49 @@ import { Song } from './Song';
 @Component({
   selector: 'app-song-list',
   templateUrl: './song-list.component.html',
-  styleUrls: ['./song-list.component.scss']
+  styleUrls: ['./song-list.component.scss'],
 })
 export class SongListComponent implements OnInit {
-
-
-  songs: Song[] =[
+  songs: Song[] = [
     {
-      nombre: "Me Porto Bonito",
-      artista: "Bad Bunny, Chencho Corleone",
+      nombre: 'Me Porto Bonito',
+      artista: 'Bad Bunny, Chencho Corleone',
       anioDeLanzamiento: 2022,
-      album: "Un Verano Sin Ti",
+      album: 'Un Verano Sin Ti',
       reproducciones: 938729948,
-      image: "assets/img/UnVeranoSinTi.jpeg"
+      image: 'https://i.scdn.co/image/ab67616d0000b27349d694203245f241a1bcaa72',
     },
     {
-      nombre: "Duki:Bzrp Music Session, Vol.50",
-      artista: "Bizarrap, Duki",
+      nombre: 'Duki:Bzrp Music Session, Vol.50',
+      artista: 'Bizarrap, Duki',
       anioDeLanzamiento: 2022,
-      album: "-",
+      album: '-',
       reproducciones: 25400628,
-      image: "assets/img/DukiBizarrap.jpeg"
+      image: 'https://i.scdn.co/image/ab67616d0000b27393c4eec56cea95c9022ed28f',
     },
     {
-      nombre: "COMO CHILLA ELLA",
-      artista: "YSY A",
+      nombre: 'COMO CHILLA ELLA',
+      artista: 'YSY A',
       anioDeLanzamiento: 2022,
-      album: "YSYSMO",
+      album: 'YSYSMO',
       reproducciones: 8253789,
-      image: "assets/img/YSYSMO.jpeg"
+      image: 'https://i.scdn.co/image/ab67616d0000b2738aaa87d1b2aa73e9e6d24dd3',
     },
     {
       nombre: "Can't Help Falling in Love",
-      artista: "Elvis Presley",
+      artista: 'Elvis Presley',
       anioDeLanzamiento: 1961,
-      album: "Blue Hawaii",
+      album: 'Blue Hawaii',
       reproducciones: 613920377,
-      image: "assets/img/BlueHawai.jpeg"
+      image: 'https://i.scdn.co/image/ab67616d0000b273c61b0774a0a43a3bd1fb9cc5',
     },
-    
-  ] 
-  
-    
+  ];
 
   constructor(private playlist: SongPlaylistService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  addToPlaylist(song: Song): void{
+  addToPlaylist(song: Song): void {
     this.playlist.addToPlaylist(song);
   }
-
 }
